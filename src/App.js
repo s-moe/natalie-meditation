bb52114584d897719f32a1bb67b7690ea5faa3e5
Sarah Moe <sarahannmoe@gmail.com>
2022-06-02 13:49:42 -0700

    added About to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./app.scss";
 import NavBar from "./components/navbar/Navbar";
+import About from "./components/about/About";
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">Natalie's site</header>
       <NavBar />
+      <About />
     </div>
   );
 }
