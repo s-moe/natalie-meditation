08d08a23c6dd1880734a7459ff5e74a4735c31b6
Sarah Moe <sarahannmoe@gmail.com>
2022-06-05 13:24:29 -0700

    added group page to app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/navbar/Navbar";
 import About from "./components/about/About";
 import Home from "./components/home/Home";
 import Individual from "./components/individual/Individual";
+import Group from "./components/group/Group";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="sections">
         <Home />
         <Individual />
+        <Group />
         <About />
       </div>
     </div>
