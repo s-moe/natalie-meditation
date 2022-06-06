e51341c20ef42e77ea5cd3da954b28a72e06dfab
Sarah Moe <sarahannmoe@gmail.com>
2022-06-06 13:51:36 -0700

    separated contact form page from contact info

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,12 +4,12 @@ import "./contact.scss";
 export default function Contact() {
   return (
     <div id="contact" className="container-fluid px-0">
-      <div class="row align-items-center content">
+      <div class="row align-items-center contact-form">
         <div class="col-md-6 order-2 order-md-1">
           <img src="" alt="" class="img-fluid" />
           Contact form
         </div>
-        <div class="col-md-6 text-center order-1 order-md-2 contact-data">
+        <div class="col-md-6 text-center order-1 order-md-2 contact-info">
           <div class="row justify-content-center">
             <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
               <h2 class="">MUSEUM OF CANDY</h2>
