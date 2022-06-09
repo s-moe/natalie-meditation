dc160c4d0f8237cc774ce23e30844ac22fcfa404
Sarah Moe <sarahannmoe@gmail.com>
2022-06-09 10:47:10 -0700

    added styling to home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,24 +4,20 @@ import "./home.scss";
 export default function Home() {
   return (
     <section className="container-fluid px-0" id="home">
-      {/* <div className="row align-items-center content">
+      <div className="row align-items-center content">
         <div className="col-md-6 order-2 order-md-1">
           <img src="./images/milk.png" alt="" className="img-fluid" />
         </div>
         <div className="col-md-6 text-center order-1 order-md-2">
           <div className="row justify-content-center">
             <div className="col-10 col-lg-8 blurb mb-5 mb-md-0">
-              <h2 className="">MUSEUM OF CANDY</h2> */}
-      {/* <img
-              src="imgs/lolli_icon.png"
-              alt=""
-              className="d-none d-lg-inline"
-            /> */}
-      {/* <p className="lead">This is content.</p>
+              <h2 className="">MUSEUM OF CANDY</h2>
+
+              <p className="lead">This is content.</p>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </section>
   );
 }
