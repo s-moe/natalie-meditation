61592ff8a6e72f3f2a0bf0bbab313494e210566f
Sarah Moe <sarahannmoe@gmail.com>
2022-06-09 10:42:18 -0700

    change class to className

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,14 +6,14 @@ import Form from "./Form.jsx";
 export default function Contact() {
   return (
     <div id="contact" className="container-fluid px-0">
-      <div class="row align-items-center contact-form">
-        <div class="col-md-6 order-2 order-md-1">
-          <img src="" alt="" class="img-fluid" />
+      <div className="row align-items-center contact-form">
+        <div className="col-md-6 order-2 order-md-1">
+          <img src="" alt="" className="img-fluid" />
           <Form />
         </div>
-        <div class="col-md-6 text-center order-1 order-md-2 contact-info">
-          <div class="row justify-content-center">
-            <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
+        <div className="col-md-6 text-center order-1 order-md-2 contact-info">
+          <div className="row justify-content-center">
+            <div className="col-10 col-lg-8 blurb mb-5 mb-md-0">
               <ContactInfo />
             </div>
           </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,25 +3,25 @@ import "./home.scss";
 
 export default function Home() {
   return (
-    <section class="container-fluid px-0">
-      <div class="row align-items-center content">
-        <div class="col-md-6 order-2 order-md-1">
-          <img src="./images/milk.png" alt="" class="img-fluid" />
+    <section className="container-fluid px-0" id="home">
+      {/* <div className="row align-items-center content">
+        <div className="col-md-6 order-2 order-md-1">
+          <img src="./images/milk.png" alt="" className="img-fluid" />
         </div>
-        <div class="col-md-6 text-center order-1 order-md-2">
-          <div class="row justify-content-center">
-            <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
-              <h2 class="">MUSEUM OF CANDY</h2>
-              {/* <img
+        <div className="col-md-6 text-center order-1 order-md-2">
+          <div className="row justify-content-center">
+            <div className="col-10 col-lg-8 blurb mb-5 mb-md-0">
+              <h2 className="">MUSEUM OF CANDY</h2> */}
+      {/* <img
               src="imgs/lolli_icon.png"
               alt=""
-              class="d-none d-lg-inline"
+              className="d-none d-lg-inline"
             /> */}
-              <p class="lead">This is content.</p>
+      {/* <p className="lead">This is content.</p>
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </section>
   );
 }
