345c7703c0405673e43bcaae2839dbbeab797efe
Sarah Moe <sarahannmoe@gmail.com>
2022-06-09 10:36:22 -0700

    adding structure to home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,5 +2,26 @@ import React from "react";
 import "./home.scss";
 
 export default function Home() {
-  return <div id="home">Home</div>;
+  return (
+    <section class="container-fluid px-0">
+      <div class="row align-items-center content">
+        <div class="col-md-6 order-2 order-md-1">
+          <img src="./images/milk.png" alt="" class="img-fluid" />
+        </div>
+        <div class="col-md-6 text-center order-1 order-md-2">
+          <div class="row justify-content-center">
+            <div class="col-10 col-lg-8 blurb mb-5 mb-md-0">
+              <h2 class="">MUSEUM OF CANDY</h2>
+              {/* <img
+              src="imgs/lolli_icon.png"
+              alt=""
+              class="d-none d-lg-inline"
+            /> */}
+              <p class="lead">This is content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
