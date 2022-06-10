80551297f9caf9408cfed9f428190c891306fd3f
Sarah Moe <sarahannmoe@gmail.com>
2022-06-10 13:01:22 -0700

    added Calendly component

---
+++ src/components/Calendly.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { PopupWidget } from "react-calendly";
+
+export default function Calendly() {
+  return (
+    <PopupWidget
+      color="#00a2ff"
+      text="Book a consultation"
+      textColor="#ffffff"
+      url="https://calendly.com/sarahannmoe"
+    />
+  );
+}
